2e5cb7f0c13aed3eb659028eb1446cf3abbee5a3
Paul Brodner <paul.brodner@ness.com>
2018-03-29 12:44:15 +0300

    adding screenshots for google

--- src/test/java/com/paulbrodner/headlessbrowsertesting/HeadlessBrowserTestingApplicationTests.java
+++ src/test/java/com/paulbrodner/headlessbrowsertesting/HeadlessBrowserTestingApplicationTests.java
@@ -2,9 +2,15 @@ package com.paulbrodner.headlessbrowsertesting;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.Augmenter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -26,6 +32,10 @@ public class HeadlessBrowserTestingApplicationTests {
 		driver = driverFactory.getObject();
 		driver.get("http://www.google.com");
 
+		WebDriver augmentedDriver = new Augmenter().augment(driver); 		
+		File source = ((TakesScreenshot)augmentedDriver).getScreenshotAs(OutputType.FILE);        
+        FileUtils.copyFile(source, new File("./target", source.getName())); 
+		
 		assertEquals("Google", driver.getTitle());
 	}
 
